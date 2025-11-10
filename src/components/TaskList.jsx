5834ab5d85c6abc4b1caee0ca49a51fa01c1e090
Jamie Bryson <jamieab98@gmail.com>
2025-11-10 17:04:08 -0500

    Completed the mark task portion

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import React, { useContext,useState } from "react";
 import { TaskContext } from "../context/TaskContext";
 
 function TaskList({query}) {
-    const [tasks, setTasks] = useState([]);
+    const {tasks, toggleComplete} = useContext(TaskContext);
     const filteredTasks = tasks.filter(task =>
     task.title.toLowerCase().includes(query.toLowerCase())
   );
@@ -14,7 +14,7 @@ function TaskList({query}) {
           <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
             {task.title}
           </span>
-          <button data-testid={task.id}>
+          <button data-testid={task.id} onClick={() => toggleComplete(task)}>
             {task.completed ? "Undo" : "Complete"}
           </button>
         </li>
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -5,8 +5,25 @@ export const TaskContext = createContext();
 export function TaskProvider({ children }) {
     const [tasks, setTasks] = useState([]);
 
+    function toggleComplete(task) {
+        fetch(`http://localhost:6001/tasks/${task.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                completed: !task.completed
+            })
+        })
+        .then((response) => response.json())
+        .then((updatedTask) => {
+            setTasks(prev => prev.map(t => (t.id == updatedTask.id ? updatedTask : t))
+        );
+    });
+}
+
     return (
-        <TaskContext.Provider value={{tasks, setTasks}}>
+        <TaskContext.Provider value={{tasks, setTasks, toggleComplete}}>
             {children}
         </TaskContext.Provider>
     );
