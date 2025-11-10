456e45b5fee4ab3598976b2beb3765a7f680fbed
Jamie Bryson <jamieab98@gmail.com>
2025-11-10 16:32:02 -0500

    I think I finished the first part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext} from "react";
 import { TaskContext } from "../context/TaskContext";
 import TaskForm from "./TaskForm";
 import SearchBar from "./SearchBar";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const {tasks, setTasks} = useContext(TaskContext)
 
   useEffect(() => {
     fetch('http://localhost:6001/tasks')
