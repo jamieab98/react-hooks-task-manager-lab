49d9a3b9828b4ea0ecf188fc8b738f76d2f8fd94
Jamie Bryson <jamieab98@gmail.com>
2025-11-10 17:32:02 -0500

    Completed the submit tasks portion

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,22 +2,26 @@ import React, { useState, useId, useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
 function TaskForm() {
+  const {addTask} = useContext(TaskContext);
   const [taskName, setTaskName] = useState("");
+  const nameId = useId();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (taskName.trim() === "") return;
     setTaskName("");
+    addTask(taskName);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>New Task:</label>
+      <label htmlFor={nameId}>New Task:</label>
       <input
         type="text"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
         placeholder="Add a new task..."
+        id={nameId}
       />
       <button type="submit">Add Task</button>
     </form>
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,9 +1,10 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useId, useState } from "react";
 
 export const TaskContext = createContext();
 
 export function TaskProvider({ children }) {
     const [tasks, setTasks] = useState([]);
+    const nameId = useId()
 
     function toggleComplete(task) {
         fetch(`http://localhost:6001/tasks/${task.id}`, {
@@ -20,10 +21,27 @@ export function TaskProvider({ children }) {
             setTasks(prev => prev.map(t => (t.id == updatedTask.id ? updatedTask : t))
         );
     });
-}
+    }
+
+    function addTask(title) {
+        fetch(`http://localhost:6001/tasks`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": title,
+                "completed": false
+            })
+        })
+        .then((response) => response.json())
+        .then((newTask) => {
+            setTasks(prev => [...prev, newTask]);
+        });
+    }
 
     return (
-        <TaskContext.Provider value={{tasks, setTasks, toggleComplete}}>
+        <TaskContext.Provider value={{tasks, setTasks, toggleComplete, addTask}}>
             {children}
         </TaskContext.Provider>
     );
